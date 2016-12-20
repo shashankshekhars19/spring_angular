d70d3c8885b64a91c66f1dfd980eac7f2c6aa56d
shekhar <shekhar@DESKTOP-0P8NDAE>
2016-12-21 00:47:21 +0530

    JDBC MySql Connection established.

--- src/main/java/com/myproject/dao/RestDAOImpl.java
+++ src/main/java/com/myproject/dao/RestDAOImpl.java
@@ -1,8 +1,15 @@
 package com.myproject.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Repository;
 
 import com.myproject.bo.Customer;
@@ -10,15 +17,38 @@ import com.myproject.bo.Customer;
 @Repository
 public class RestDAOImpl implements RestDAO {
 
+	@Autowired
+	@Qualifier("mysqlJdbcTemplate")
+	private JdbcTemplate mysqlTemplate;
+
 	@Override
 	public Customer getCustomer() {
 		// TODO Auto-generated method stub
-		Customer customer = new Customer();
-		customer.setName("shashank");
-		customer.setPhoneNo("919019239");
-		customer.setState("BIHAR");
-		customer.setZipCode("813221");
-		return customer;
+
+		String sql = "SELECT * FROM customer WHERE name LIKE 'shashank'";
+		return mysqlTemplate.query(sql, new ResultSetExtractor<Customer>() {
+
+			@Override
+			public Customer extractData(ResultSet rs) throws SQLException, DataAccessException {
+				if (rs.next()) {
+					Customer customer = new Customer();
+					customer.setName(rs.getString("name"));
+					customer.setPhoneNo(rs.getString("phoneNo"));
+					customer.setZipCode(rs.getString("zipCode"));
+					customer.setState(rs.getString("state"));
+
+					return customer;
+				}
+
+				return null;
+			}
+
+		});
+		/*
+		 * customer.setName("shashank"); customer.setPhoneNo("919019239");
+		 * customer.setState("BIHAR"); customer.setZipCode("813221"); return
+		 * customer;
+		 */
 	}
 
 	@Override
@@ -29,7 +59,7 @@ public class RestDAOImpl implements RestDAO {
 		customer.setPhoneNo("919019239");
 		customer.setState("BIHAR");
 		customer.setZipCode("813221");
-		
+
 		Customer customer1 = new Customer();
 		customer1.setName("shekhar");
 		customer1.setPhoneNo("9124219239");
@@ -38,8 +68,8 @@ public class RestDAOImpl implements RestDAO {
 		List<Customer> list = new ArrayList<>();
 		list.add(customer1);
 		list.add(customer);
-		
- 		return list;
+
+		return list;
 	}
 
 	@Override
